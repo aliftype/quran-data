5d19591c8b369ff69509b4f475a741365ee63d86
Khaled Hosny <khaledhosny@eglug.org>
2016-07-10 22:47:45 +0300

    Use the metadata file

--- tools/build_html.py
+++ tools/build_html.py
@@ -1,4 +1,5 @@
 import sys
+import glob
 from os import path
 from jinja2 import Template
 
@@ -18,9 +19,18 @@ htmlTemplate = Template("""<html>
       font-size: 18pt;
       text-align: center;
     }
+    .sura-name {
+      font-family: Reem Kufi;
+      font-size: 24pt;
+      font-feature-settings: "cv01";
+      text-align: center;
+    }
     </style>
   </head>
   <body>
+    <div class="sura-name">
+    سورة {{name}}\t{{place}}
+    </div>
     {%- if basmala %}
     <p class="basmala">&#xfdfd;</p>
     {% endif -%}
@@ -31,20 +41,33 @@ htmlTemplate = Template("""<html>
 </html>
 """)
 
-def buildPage(textfile):
+def BuildPage(textfile, metadata):
     text = textfile.read().strip()
     text = text.replace(" \u06dd", "\u00a0\u06dd")
     num = int(path.splitext(path.basename(textfile.name))[0])
-    basmala = num not in (1, 9)
-    html = htmlTemplate.render(text=text, basmala=basmala)
+    name, place, basmala = metadata[num]
+    html = htmlTemplate.render(text=text, name=name, place=place, basmala=basmala)
 
     return html
 
+def ReadMetadata(filename):
+    metadata = {}
+    with open(filename) as metafile:
+        lines = [l.strip().split("\t") for l in metafile.readlines()]
+        for num, line in enumerate(lines):
+            num = num + 1
+            metadata[num] = [line[0], line[1], True]
+            if len(line) == 3:
+                metadata[num][2] = False
+    return metadata
+
 def main():
-    filenames = sys.argv[1:]
+    dirname = sys.argv[1]
+    filenames = glob.glob(dirname + "/???.txt")
+    metadata = ReadMetadata(dirname + "/meta.txt")
     for filename in filenames:
         with open(filename, "r") as textfile:
-            html = buildPage(textfile)
+            html = BuildPage(textfile, metadata)
             with open(filename.replace(".txt", ".html"), "w") as htmlfile:
                 htmlfile.write(html)
 
