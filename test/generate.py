166a02926f7b3a9e57c860762fde0c0d27b41181
Khaled Hosny <khaledhosny@eglug.org>
2017-01-21 18:05:15 +0200

    Pretty print the JSON output

--- test/generate.py
+++ test/generate.py
@@ -1,8 +1,9 @@
+import json
 import glob
 import os
 import subprocess
 
-for text_filename in glob.glob("../quran/???.txt"):
+for text_filename in sorted(glob.glob("../quran/???.txt")):
   base_filename = os.path.basename(text_filename)
   json_filename = os.path.splitext(base_filename)[0] + ".json"
   print("Generating %s ⇒ %s" % (text_filename, json_filename))
@@ -14,5 +15,5 @@ for text_filename in glob.glob("../quran/???.txt"):
                         check=True, stdout=subprocess.PIPE)
   json_file = open(json_filename, "w")
   for line in proc.stdout.split():
-      json_file.write(line.decode("ascii"))
+      json_file.write(json.dumps(json.loads(line), indent=1))
       json_file.write("\n")
