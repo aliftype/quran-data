05c7a250910276bd19842be55ee4a0ddb5803c8d
Khaled Hosny <khaledhosny@eglug.org>
2017-02-12 01:42:08 +0200

    Output each line to a separate file
    Easer to process with JSON tools since HarfBuzz output is really a
    standalone JSON file for each line.

--- test/generate.py
+++ test/generate.py
@@ -7,13 +7,15 @@ import subprocess
 
 for text_filename in sorted(glob.glob("../quran/???.txt")):
   base_filename = os.path.basename(text_filename)
-  json_filename = os.path.splitext(base_filename)[0] + ".json"
-  print("Generating %s ⇒ %s" % (text_filename, json_filename))
+  json_dirname = os.path.splitext(base_filename)[0]
+  print("Generating %s ⇒ %s" % (text_filename, json_dirname))
   proc = subprocess.run(["hb-shape",
                          "--font-file=amiri-quran.ttf",
                          "--text-file=%s" % text_filename,
                          "--output-format=json",
                          "--cluster-level=1"],
                         check=True, stdout=subprocess.PIPE)
-  json_file = open(json_filename, "w")
-  json_file.write(proc.stdout.decode("utf-8"))
+  os.makedirs(json_dirname, exist_ok=True)
+  for i, line in enumerate(proc.stdout.split()):
+    with open(json_dirname + "/%03d.json" % i, "w") as json_file:
+      json_file.write(line.decode("utf-8"))
